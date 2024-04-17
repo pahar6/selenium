0e0560da4051e0c2f33d3d62ef8fe5127fdbc477
pahar6 <167367840+pahar6@users.noreply.github.com>
2024-04-17 23:53:04 +0200

    Update __init__.py

--- auto_selenium/__init__.py
+++ auto_selenium/__init__.py
@@ -1,1 +1,14 @@
+from .browser.base import Browser
+from .browser.mixins import CookieMixin, ProxyMixin
+from .browser.settings import BrowserArgs, BrowserSettings, by
 
+__all__ = (
+    "Browser",
+    "BrowserArgs",
+    "BrowserSettings",
+    "CookieMixin",
+    "ProxyMixin",
+    "by",
+)
+
+__version__ = '0.1.0'
